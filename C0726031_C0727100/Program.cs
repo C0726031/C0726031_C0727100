501a4fd75f8068e106b1762b1ff278c2c2171737
visualstudio <visualstudio@122-PC22>
2019-03-15 21:06:27 -0400

    name list

--- C0726031_C0727100/Program.cs
+++ C0726031_C0727100/Program.cs
@@ -6,12 +6,28 @@ using System.Threading.Tasks;
 
 namespace C0726031_C0727100
 {
-    class Program
 
+    public delegate void Sort_DataStructure();
+
+    class Program
     {
         static void Main(string[] args)
         {
-            int i = 10;
+            Student s = new Student();
+            s.Run();
+        }
+    }
+
+    class Student
+    {
+        List<string> StudentList = new List<string>();
+        String Student_name;
+        String StudentID;
+        double StudentGPA;
+
+        internal void Run()
+        {
+            throw new NotImplementedException();
         }
     }
 }
